f7fbdab4dac26508ceab25b6222e9878e20681a7
Altyn <altynberdiyeva99@gmail.com>
2021-03-06 10:19:40 +0500

    Initial

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <div class="img-slider">
+      <img src="@/assets/2.jpg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>  
+  .main{
+    position: relative;
+    width: 100%;
+  }
+  .img-slider{
+    width: 100%;
+    min-height: 100vh;  
+    height: auto;
+    position: relative;
+  }
+  .img-slider img{
+    position: absolute;
+    width: 100%;
+    height: auto;
+  }
+</style>
